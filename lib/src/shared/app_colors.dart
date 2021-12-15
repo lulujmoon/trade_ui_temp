0c9debb54c94e5d8d785abea4c264dfb9c4da864
junghyunmoon <lulujmoon@gmail.com>
2021-12-15 14:02:59 +0900

    컬러 수정

--- lib/src/shared/app_colors.dart
+++ lib/src/shared/app_colors.dart
@@ -5,29 +5,17 @@ const Color kcMediumGreyColor = Color(0xff868686);
 const Color kcLightGreyColor = Color(0xffe5e5e5);
 const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
 
+const Color lightGreyColor1 = Color(0xfff1f1f1);
+const Color lightGreyColor2 = Color(0xffdcdcdc);
+const Color greyColor = Color(0xff26272C);
+const Color lightBlueColor1 = Color(0xffffffff);
+const Color lightBlueColor2 = Color(0xfff8f9ff);
+const Color lightBlueColor3 = Color(0xffe9ecf3);
+const Color lightBlueColor4 =  Color(0xffE8F0FE);
+const Color lightBlueColor5 = Color(0xffd6ddf2);
+const Color dustBlueColor1 = Color(0xffc8cfdb);
+const Color dustBlueColor2 = Color(0xffb9bfd1);
+const Color dustBlueColor3 = Color(0xff77849F);
 const Color primaryBlueColor = Color(0xff2F67FC);
-const Color lightGreyColor = Color(0xffe0e0e0);
-
-// button
-const Color loginBtnGreyColor = Color(0xffD6DDF2);
-const Color loginBtnBorderGreyColor = Color(0xffB9BFD1);
-const Color btnBorderGreyColor = Color(0xffDCDCDC);
-const Color btnNavyColor = Color(0xff081647);
-const Color btnGreyColor = Color(0xffE9ECF3);
-const Color btnBorderGrey2Color = Color(0xfff1f1f1); // 매출처 추가, 일정 추가하기
-
-//text
-const Color smallTitleBlueColor = Color(0xff77849F);
-const Color hintTextGreyColor = Color(0xff26272C);
-const Color textOrangeColor = Color(0xffEE6D00);
-
-//background
-const Color joinBgColorColor = Color(0xffF8F9FF);
-
-//fill
-const Color inputWhiteColor = Color(0xffffffff);
-const Color innerCellBlueColor =  Color(0xffE8F0FE);
-
-//border
-const Color inputBorderGreyColor = Color(0xffDADADA);
-const Color tradeBorderGreyColor = Color(0xffC8CFDB);
+const Color navyColor = Color(0xff081647);
+const Color orangeColor = Color(0xffEE6D00);
