9970e75180979ddd1ba59b658d78bda3e3ce6838
DESKTOP-IUO5HV7\Frentree <ldg0315@github.com>
2021-12-15 11:10:05 +0900

    test

--- tradestmt_web/lib/main.dart
+++ tradestmt_web/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo2',
       theme: ThemeData(
         primaryColor: kcPrimaryColor,
       ),
