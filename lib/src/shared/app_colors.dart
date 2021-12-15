a9f62504ec3a84130052bca6b73221b0002dc726
DESKTOP-IUO5HV7\Frentree <ldg0315@github.com>
2021-12-15 12:20:01 +0900

    컬러 추가

--- lib/src/shared/app_colors.dart
+++ lib/src/shared/app_colors.dart
@@ -4,3 +4,30 @@ const Color kcPrimaryColor = Color(0xff22A45D);
 const Color kcMediumGreyColor = Color(0xff868686);
 const Color kcLightGreyColor = Color(0xffe5e5e5);
 const Color kcVeryLightGreyColor = Color(0xfff2f2f2);
+
+const Color primaryBlueColor = Color(0xff2F67FC);
+const Color lightGreyColor = Color(0xffe0e0e0);
+
+// button
+const Color loginBtnGreyColor = Color(0xffD6DDF2);
+const Color loginBtnBorderGreyColor = Color(0xffB9BFD1);
+const Color btnBorderGreyColor = Color(0xffDCDCDC);
+const Color btnNavyColor = Color(0xff081647);
+const Color btnGreyColor = Color(0xffE9ECF3);
+const Color btnBorderGrey2Color = Color(0xfff1f1f1); // 매출처 추가, 일정 추가하기
+
+//text
+const Color smallTitleBlueColor = Color(0xff77849F);
+const Color hintTextGreyColor = Color(0xff26272C);
+const Color textOrangeColor = Color(0xffEE6D00);
+
+//background
+const Color joinBgColorColor = Color(0xffF8F9FF);
+
+//fill
+const Color inputWhiteColor = Color(0xffffffff);
+const Color innerCellBlueColor =  Color(0xffE8F0FE);
+
+//border
+const Color inputBorderGreyColor = Color(0xffDADADA);
+const Color tradeBorderGreyColor = Color(0xffC8CFDB);
